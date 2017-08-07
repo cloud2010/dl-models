aaf2650aa0d1848c0ecac59f8bb96803da6c568c
Min Liu <capt.liu@foxmail.com>
2017-08-08 02:50:14 +0800

    add seaborn support

--- svm-learning.py
+++ svm-learning.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import learning_curve
 from sklearn.datasets import load_digits
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 digits = load_digits()
@@ -18,6 +19,8 @@ train_sizes, train_loss, test_loss = learning_curve(
 train_loss_mean = -np.mean(train_loss, axis=1)
 test_loss_mean = -np.mean(test_loss, axis=1)
 
+# Activate Seaborn Style
+sns.set()
 plt.plot(train_sizes, train_loss_mean, 'o-', color="r",
          label="Training")
 plt.plot(train_sizes, test_loss_mean, 'o-', color="g",
