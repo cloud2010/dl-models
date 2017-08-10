901fdee3646a4427e4c89d3f2dd64efca77207ba
Min Liu <capt.liu@foxmail.com>
2017-08-11 00:32:57 +0800

    add some tf test code

--- numpy-test.py
+++ numpy-test.py
@@ -2,7 +2,10 @@
 Numpy learning
 """
 import numpy as np
+import tensorflow as tf
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # 导入CSV数据
 TRAIN_NITRATION = os.path.join(os.path.dirname(
     __file__), "d:\\train_nitration_941_standard.csv")
@@ -39,21 +42,25 @@ def random_sample(train_set, size=10, isReplace=True):
     instance_num = np.arange(1, train_set.shape[0])
     # 随机生成抽样行
     sample_row = np.random.choice(instance_num, size, isReplace)
-
-    # 输出抽样行矩阵切片（列数为训练集的列数）
+    # 输出抽样行矩阵切片（切片列数为训练集的列数）
     sample_set = np.empty(shape=[train_set.shape[1], ])
-    # print(sample_set.shape)
-    
     # 循环抽取对应行构建出抽样矩阵
     for i in sample_row:
         sample_set = np.vstack((sample_set, train_set[i, :]))
     # 剔除第一行初始值
     sample_set = np.delete(sample_set, 0, 0)
-    
     # 返回抽样的分类结果和输入数据（通常是特征值），默认第一列为target（分类结果）
     target = sample_set[:, 0]
     features = sample_set[:, 1:] 
     return (target, features)
 
-batch_x, batch_y = random_sample(train_nitration_set, 4, True)
-print(batch_y)
+# 测试抽样结果
+batch_x, batch_y = random_sample(train_nitration_set, 10, True)
+print("batch_x:", batch_x)
+
+# 开始测试 TensorFlow 赋值
+node1 = tf.constant(batch_x, dtype=tf.float32)
+print("node1:", node1)
+
+sess = tf.Session()
+print("node1:", sess.run(node1))
