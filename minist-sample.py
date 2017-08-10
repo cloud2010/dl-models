83b3b1f81349d20c3671a2100946fc46ac37a435
Min Liu <capt.liu@foxmail.com>
2017-08-10 17:05:16 +0800

    fix print fun err

--- minist-sample.py
+++ minist-sample.py
@@ -65,4 +65,4 @@ correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 # 最后用测试集 test data 验证模型准确率
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+print("The MINIST Predict Accuracy: ", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
