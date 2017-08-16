e7b695d4fa2f9b2297635a5c9b2ebd5a0cf516e9
Min Liu <capt.liu@foxmail.com>
2017-08-17 02:25:16 +0800

    dict update

--- numpy-test/dict-test.py
+++ numpy-test/dict-test.py
@@ -14,7 +14,13 @@ weights = {"out": "output layer"}
 print(weights)
 
 # 动态添加字典
-for i in range(int(n_hiddens)):
+for i in range(1, int(n_hiddens) + 1):
     weights["h" + str(i)] = "hidden layer{0}".format(i)
 
 print(weights)
+
+biases = {0: 0}
+for i in range(1, n_hiddens + 1):
+    biases[i] = i**2
+
+print(biases)
