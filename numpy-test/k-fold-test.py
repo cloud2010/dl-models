5ce048f8c51a438c685d974fcd93113189df5491
Min Liu <capt.liu@foxmail.com>
2017-08-20 15:49:29 +0800

    add logging module

--- numpy-test/k-fold-test.py
+++ numpy-test/k-fold-test.py
@@ -8,6 +8,21 @@ proportion of samples on each side of the train / test split.
 import numpy as np
 from sklearn.model_selection import ShuffleSplit
 import os
+import logging  # 导入日志模块
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='d://train.log',
+                    filemode='w')
+
+# 定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误
+# 并将其添加到当前的日志处理对象
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # 导入CSV数据
