e80270dc50cef992fcee16d36939c27f8ddd2c0e
Capt.Liu <capt.liu@foxmail.com>
2017-09-05 14:13:09 +0800

    add snn module and update setttings.json

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'Min'
+
+import sys
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, world!')
+    elif len(args) == 2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+
+if __name__ == '__main__':
+    test()
