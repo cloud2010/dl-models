98ebd896d4e7a6f8c38b331ac532ecc0e0d76f4e
Min Liu <capt.liu@foxmail.com>
2017-09-20 01:46:17 +0800

    code cleanup

--- snn/selu.py
+++ snn/selu.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 '''
 Tensorflow Implementation of the Scaled ELU function and Dropout
+Ref: https://github.com/bioinf-jku/SNNs/blob/master/selu.py
 '''
 
 from __future__ import absolute_import, division, print_function
 import numbers
-import tensorflow as tf
-# from tensorflow.contrib import layers
+from tensorflow.contrib import layers
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_util
@@ -14,8 +14,12 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import random_ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.layers import utils
+import tensorflow as tf
+
+# (1) scale inputs to zero mean and unit variance
 
 
+# (2) use SELUs
 def selu(x):
     with ops.name_scope('elu') as scope:
         alpha = 1.6732632423543772848170429916717
@@ -23,6 +27,12 @@ def selu(x):
         return scale * tf.where(x >= 0.0, x, alpha * tf.nn.elu(x))
 
 
+# (3) initialize weights with stddev sqrt(1/n)
+# e.g. use:
+initializer = layers.variance_scaling_initializer(factor=1.0, mode='FAN_IN')
+
+
+# (4) use this dropout
 def dropout_selu(x, rate, alpha=-1.7580993408473766, fixedPointMean=0.0, fixedPointVar=1.0,
                  noise_shape=None, seed=None, name=None, training=False):
     """Dropout to a value with rescaling."""
@@ -38,7 +48,7 @@ def dropout_selu(x, rate, alpha=-1.7580993408473766, fixedPointMean=0.0, fixedPo
         keep_prob.get_shape().assert_is_compatible_with(tensor_shape.scalar())
 
         alpha = ops.convert_to_tensor(alpha, dtype=x.dtype, name="alpha")
-        keep_prob.get_shape().assert_is_compatible_with(tensor_shape.scalar())
+        alpha.get_shape().assert_is_compatible_with(tensor_shape.scalar())
 
         if tensor_util.constant_value(keep_prob) == 1:
             return x
@@ -51,8 +61,8 @@ def dropout_selu(x, rate, alpha=-1.7580993408473766, fixedPointMean=0.0, fixedPo
         binary_tensor = math_ops.floor(random_tensor)
         ret = x * binary_tensor + alpha * (1 - binary_tensor)
 
-        a = tf.sqrt(fixedPointVar / (keep_prob * ((1 - keep_prob)
-                                                  * tf.pow(alpha - fixedPointMean, 2) + fixedPointVar)))
+        a = math_ops.sqrt(fixedPointVar / (keep_prob * ((1 - keep_prob)
+                                                        * math_ops.pow(alpha - fixedPointMean, 2) + fixedPointVar)))
 
         b = fixedPointMean - a * \
             (keep_prob * fixedPointMean + (1 - keep_prob) * alpha)
