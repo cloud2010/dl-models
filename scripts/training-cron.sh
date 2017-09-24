6d1ae9a6ad2b2e1493038551425395b00066c715
Min@Home <liumin@shmtu.edu.cn>
2017-09-24 17:22:03 +0800

    sh cleanup

--- scripts/training-cron.sh
+++ scripts/training-cron.sh
@@ -3,14 +3,14 @@
 # default setting
 if [ $# -eq 1 ]
 then
-	echo "default test datasets is 941."
-	datasets=941
-	# echo "Usage : $0 number"
-	# exit 1
+    echo "default test datasets is 941."
+    datasets=941
+    # echo "Usage : $0 number"
+    # exit 1
 fi
 
-for ((i=1; i<=$datasets; i++)) 
+for ((i=1; i<=$datasets; i++))
 do
-	# gdbt caculate
-	# echo "md $i Process running...`python3.6 gdbt.py -cv 10 -md $i heart_scale 1>heart_scale-cv-10-md-$i.log 2>&1`"
+    # gdbt caculate
+    # echo "md $i Process running...`python3.6 gdbt.py -cv 10 -md $i heart_scale 1>heart_scale-cv-10-md-$i.log 2>&1`"
 done
