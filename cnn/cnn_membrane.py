ae45978d421ffd0c7e81f3d7fe6a5808def6b596
Min@Home <liumin@shmtu.edu.cn>
2017-11-05 16:11:14 +0800

    implement cnn

--- cnn/cnn_membrane.py
+++ cnn/cnn_membrane.py
@@ -14,12 +14,13 @@ from __future__ import print_function
 import os
 import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Membrane Parameters
 N_CLASSES = 5  # CHANGE HERE, total number of classes
-DATA_HEIGHT = 5000  # CHANGE HERE, the membrane's height
+DATA_HEIGHT = 2000  # CHANGE HERE, the membrane's height
 DATA_WIDTH = 20  # CHANGE HERE, the membrane's width
 CHANNELS = 1  # change to 1 if grayscale
 
@@ -45,7 +46,7 @@ def read_membrane_datasets(dataset_path):
             f.close()  # Membrane 文件完整路径
             # 登记相关信息至训练集列表
             membrane_paths.append(filepath)
-            membrane_labels.append(m_class_info[1])
+            membrane_labels.append(int(m_class_info[1]) - 1)
     return membrane_paths, membrane_labels
 
 
@@ -68,11 +69,19 @@ def read_membrane_data(file_path):
                 # 逐行存入矩阵信息
                 result.append(line.split())
     # 转换至 numpy array 格式
-    mat = np.array(result, dtype=np.int16)
-
-    # 矩阵填充 padding with 0
-    new_mat = np.lib.pad(
-        mat, ((0, DATA_HEIGHT - mat.shape[0]), (0, 0)), 'constant', constant_values=0)
+    mat = np.array(result, dtype=np.float)
+    if mat.shape[0] > DATA_HEIGHT:
+        # 超出 Max Height 则矩阵做截断
+        new_mat = mat[0:DATA_HEIGHT, ]
+    else:
+        # 矩阵填充 padding with 0
+        new_mat = np.lib.pad(mat, ((
+            0, DATA_HEIGHT - mat.shape[0]), (0, 0)), 'constant', constant_values=0)
+    # 标准化处理
+    scale = StandardScaler()
+    scale.fit(new_mat)
+    new_mat = scale.transform(new_mat)[:,:,np.newaxis]
+    
     return new_mat
 
 
@@ -95,29 +104,30 @@ def conv_net(x, n_classes, dropout, reuse, is_training):
         # data input is a 1-D vector of (DATA_HEIGHT*DATA_WIDTH) features
         # Reshape to format [Height x Width x Channel]
         # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-        # -1 代表每次可以自定义样本输入数量
+        # 特征矩阵调整为 4-D 输入，-1 代表每次可以自定义样本输入数量
+        # 在给定的 4-D input与 filter下计算2D卷积
         # x = tf.reshape(x, shape=[-1, DATA_HEIGHT, DATA_WIDTH, 1])
 
         # Convolution Layer 1 with 32 filters and a kernel size of 5
         # 卷积层1：卷积核大小为 5x5，卷积核数量为 32， 激活函数使用 RELU
-        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
+        conv1 = tf.layers.conv2d(x, 32, 5, padding="same", activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         # 采用 2x2 维度的最大化池化操作，步长为2
-        conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+        pool1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
         # Convolution Layer 2 with 64 filters and a kernel size of 3
         # 卷积层2：卷积核大小为 3x3，卷积核数量为 64， 激活函数使用 RELU
-        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+        conv2 = tf.layers.conv2d(pool1, 64, 3, padding="same", activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         # 采用 2x2 维度的最大化池化操作，步长为2
-        conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+        pool2 = tf.layers.max_pooling2d(conv2, 2, 2)
 
         # Flatten the data to a 1-D vector for the fully connected layer
-        fc1 = tf.contrib.layers.flatten(conv2)
+        fc1 = tf.contrib.layers.flatten(pool2)
 
         # Fully connected layer (in contrib folder for now)
         # 全链接层具有1024神经元
-        fc1 = tf.layers.dense(fc1, 1024)
+        fc1 = tf.layers.dense(fc1, units=1024)
         # Apply Dropout (if is_training is False, dropout is not applied)
         # 对全链接层的数据加入dropout操作，防止过拟合
         fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
@@ -127,12 +137,12 @@ def conv_net(x, n_classes, dropout, reuse, is_training):
         # Because 'softmax_cross_entropy_with_logits' already apply softmax,
         # we only apply softmax to testing network
         # 输出层 (Softmax层)，对dropout层的输出Tensor，执行分类操作
-        out = tf.nn.softmax(out) if not is_training else out
+        # out = tf.nn.softmax(out) if not is_training else out
 
     return out
 
 
-def run_model(d_path, l_rate, n_steps, b_size, k_prob, folds, seed=None):
+def run_model(d_path, l_rate, n_steps, b_size, d_rate, folds, seed=None):
     """运行 CNN 模型
 
     Args:
@@ -144,41 +154,44 @@ def run_model(d_path, l_rate, n_steps, b_size, k_prob, folds, seed=None):
       folds: 交叉验证次数
       seed: 随机数种子
     """
-    # tf Graph input
-    # 占位符
-    X = tf.placeholder(tf.float32)
-    y = tf.placeholder(tf.int16)
-    keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
+    # 特征和分类矩阵占位符
+    X = tf.placeholder(tf.float32, shape=[None, DATA_HEIGHT, DATA_WIDTH, 1])
+    y = tf.placeholder(tf.int32)
+    dropout = tf.placeholder(tf.float32)  # dropout (keep probability)
 
     # 读取数据
     traning_set, training_labels = read_membrane_datasets(d_path)
-    sample = read_membrane_data(traning_set[0])
-    tf_sample = tf.convert_to_tensor(sample, dtype=tf.int16)
-    tf_sample_file = tf.convert_to_tensor(traning_set[0], dtype=tf.string)
-    tf_sample_label = tf.convert_to_tensor(int(training_labels[0]), dtype=tf.int16)
+
+    # 选取一部分索引进行测试
+    sample_list = read_membrane_data(traning_set[b_size]).reshape(1, DATA_HEIGHT, DATA_WIDTH, 1)
+    label_list = training_labels[b_size]
+    for i in range(b_size):
+        sample_list = np.vstack((sample_list, read_membrane_data(traning_set[i]).reshape(1, DATA_HEIGHT, DATA_WIDTH, 1)))
+        label_list = np.hstack((label_list, training_labels[i]))
+    # print(sample_list[0])
+    print(sample_list.shape)
+    print(label_list.shape)
 
     # Because Dropout have different behavior at training and prediction time, we
     # need to create 2 distinct computation graphs that share the same weights.
     # Create a graph for training
-    logits_train = conv_net(X, N_CLASSES, keep_prob,
-                            reuse=False, is_training=True)
+    logits_train=conv_net(X, N_CLASSES, dropout, reuse=False, is_training=True)
     # Create another graph for testing that reuse the same weights
-    logits_test = conv_net(X, N_CLASSES, keep_prob,
-                           reuse=True, is_training=False)
+    # logits_test = conv_net(X, N_CLASSES, keep_prob, reuse=True, is_training=False)
 
     # Define loss and optimizer (with train logits, for dropout to take effect)
     # sparse_softmax_cross_entropy_with_logits() do not use the one hot version of labels
-    loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    loss_op=tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=logits_train, labels=y))
-    optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
-    train_op = optimizer.minimize(loss_op)
+    optimizer=tf.train.AdamOptimizer(learning_rate=l_rate)
+    train_op=optimizer.minimize(loss_op)
 
     # Evaluate model (with test logits, for dropout to be disabled)
-    correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.cast(y, tf.int16))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    correct_pred=tf.equal(tf.argmax(logits_train, 1), tf.cast(y, tf.int64))
+    accuracy=tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     # Initialize the variables (i.e. assign their default value)
-    init = tf.global_variables_initializer()
+    init=tf.global_variables_initializer()
 
     # Saver object
     # saver = tf.train.Saver()
@@ -188,10 +201,15 @@ def run_model(d_path, l_rate, n_steps, b_size, k_prob, folds, seed=None):
 
         # Run the initializer
         sess.run(init)
-        # print(tf_sample)
-        # print(sess.run([tf_sample, tf_sample_label, tf_sample_file]))
+        for i in range(1, n_steps + 1):
+            sess.run(train_op, feed_dict={X: sample_list, y: label_list, dropout: d_rate})
+            _, loss, acc = sess.run([logits_train, loss_op, accuracy], feed_dict={X: sample_list, y: label_list, dropout: d_rate})
+            print("\nOutput:", _)
+            print("\nTraining Step " + str(i) + ", Training Accuracy = " + "{:.6f}".format(acc) + ", Minibatch Loss = " + "{:.6f}".format(loss))
+        print("\nTraining Finished!")
 
         # Save your model
         # saver.save(sess, 'membrane_tf_model')
 if __name__ == "__main__":
-    run_model("D:\\datasets\\membrane", l_rate=0.01, n_steps=10, b_size=100, k_prob=0.8, folds=10)
+    run_model("D:\\datasets\\membrane", l_rate=0.01,
+              n_steps=10, b_size=100, d_rate=0.2, folds=10)
--- files-ops/datasets_management.py
+++ files-ops/datasets_management.py
@@ -4,7 +4,8 @@ We will show how to load Membrane data into numpy array.
 import os
 import numpy as np
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-
+from PIL import Image
+from sklearn.preprocessing import StandardScaler
 __author__ = 'Min'
 
 
@@ -26,21 +27,46 @@ if __name__ == "__main__":
                 # 逐行存入矩阵信息
                 result.append(line.split())
     # python list convert to numpy array
-    mat = np.array(result, dtype=np.int16)
-    print("\nMembrane name: {0}, Class: {1}".format(
-        labels[0].lstrip('>'), labels[1]))
-    print("\nMatrix shape:", mat.shape)
-    print("\nFirst line:", mat[1])
+    mat = np.array(result, dtype=np.float)
+    
+
     # padding array with 0, number of values padded to the footer
-    new_mat = np.lib.pad(
-        mat, ((0, 5000 - mat.shape[0]), (0, 0)), 'constant', constant_values=0)
+    if mat.shape[0] > 2000:
+        mat = mat[0:2000, ]
+    else:
+        new_mat = np.lib.pad(
+            mat, ((0, 2000 - mat.shape[0]), (0, 0)), 'constant', constant_values=0)
+    scale = StandardScaler()
+    scale.fit(new_mat)
+    print("\nScale Mean:", scale.mean_)
     print("\n", new_mat)
+    new_mat = scale.transform(new_mat)
+    print("\nAfter Standardization:\n", new_mat)
     print("\n", new_mat.shape)
+    new_mat = new_mat[:, :, np.newaxis]
+    print(new_mat.shape)
+    
+    # img = Image.fromarray((new_mat * 255).astype(np.uint8))
+    # img.save('test.jpg')
+    # img.show()
+    # img.save("test.jpg")
+    # print("\nMembrane name: {0}, Class: {1}".format(
+    #     labels[0].lstrip('>'), labels[1]))
+    # print("\nMatrix shape:", mat.shape)
+    # print("\nFirst line:", mat[1])
 
 # 矩阵 padding 测试，矩阵尾部以 0 代替
-a = np.arange(9).reshape((3,3))
-b = np.lib.pad(a, ((0, 5000 - 3), (0, 0)), 'constant', constant_values=0)
-print("\nBefore padding:", a.shape)
-print("\n", a)
-print("\nAfter padding:", b.shape)
-print("\n", b)
+# a = np.arange(9).reshape((3, 3))
+# b = np.lib.pad(a, ((0, 5000 - 3), (0, 0)), 'constant', constant_values=0)
+# print("\nBefore padding:", a.shape)
+# print("\n", a)
+# print("\nAfter padding:", b.shape)
+# print("\n", b)
+# c = b[0:5, ].reshape(1, 5, 3, 1)
+# print("\n", c)
+# c = np.append(c, b[6:11, ].reshape(1, 5, 3, 1), axis=0)
+# print("\n", c.shape)
+# a = np.array([1])
+# b = np.array([2])
+# a = np.hstack((a, b))
+# print(a)
