e2ec730b9c10ca07f2fa2a77379ab046b695e626
Capt.Liu <capt.liu@foxmail.com>
2018-04-18 10:40:59 +0800

    update gitignore

---
+++ capsNet/torch-learn.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+from pathlib import Path
+
+FILE = "peptide_caps_net.csv"  # 数据集名称
+DATASETS = Path(__file__).parent.parent.joinpath("datasets", FILE)  # 构建路径
+
+print(DATASETS)
--- plt-learn.py
+++
@@ -1,9 +0,0 @@
-import  matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 20)
-y = np.sin(x)
-
-plt.plot(x, y)
-plt.grid(True)
-plt.show()
