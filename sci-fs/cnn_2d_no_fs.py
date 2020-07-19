c3abed858b0ff8a71f577f8b267a17dcaac28b94
liumin@home <liumin@shmtu.edu.cn>
2020-07-19 16:57:59 +0800

    update init func

--- sci-fs/cnn_2d_no_fs.py
+++ sci-fs/cnn_2d_no_fs.py
@@ -57,7 +57,7 @@ class ConvNet(nn.Module):
         out = self.fc(out)
         return out
 
-
+@torch.no_grad()
 def init_model(m):
     r"""Model weights and bias initialization.
 
@@ -65,8 +65,9 @@ def init_model(m):
         m: Conv2d or Linear model
     """
     if (type(m) == nn.Conv2d) or (type(m) == nn.Linear):
-        nn.init.xavier_uniform_(m.weight)
-        m.bias.data.fill_(0)
+        # nn.init.xavier_uniform_(m.weight)
+        m.weight.fill_(1.0)
+        m.bias.fill_(0.0)
 
 
 def bi_model_evaluation(y_true, y_pred):
